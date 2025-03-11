b8101b93c5239caf98dd5cea0f31e719beffe154
franztao <franztaoheng@gmail.com>
2025-03-11 15:20:44 +0800

    pls

--- tests/metagpt/roles/test_program_execute.py
+++ tests/metagpt/roles/test_program_execute.py
@@ -67,7 +67,7 @@ async def test_program_execute():
     memory_long = content + "\n" + "---" * 1 + "\n文件地址" + p2 + "\n文件中内容:\n" + ''.join(ls) + "\n" + "---" * 1
 
     requirement = prompt.format(goal=goal, memory_short=memory_short,
-                                memory_long=memory_long[0:len(memory_long) - 10000], output_demand=output_demand)
+                                memory_long=memory_long[0:len(memory_long) - 15000], output_demand=output_demand)
 
     di = ProgramExecute(react_mode="react", tools=["shell_tool"])
     rsp = await di.run(requirement)
