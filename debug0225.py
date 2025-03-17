bf26518a0184f22628d34eebacfe3f640aea2697
franztao <franztaoheng@gmail.com>
2025-03-17 10:27:11 +0800

    pls

--- debug0225.py
+++ debug0225.py
@@ -20,4 +20,40 @@ if __name__ == '__main__':
     # asyncio.run(main(requirement))
     # r='\u8981\u4e0d\u54b1\u4eec\u6362\u4e2a\u8bdd\u9898\uff1f'
     # print(r)
-    f1()
+    # f1()
+    import os
+    from openai import OpenAI
+
+    client = OpenAI(
+        # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
+        api_key="sk-f7f3039f52e3402bbafda926f4da7cb3",
+        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    )
+    completion = client.chat.completions.create(
+        model="qwen-max",
+        # 此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+        messages=[
+            {'role': 'system', 'content': 'You are a helpful assistant.'},
+            {'role': 'user', 'content': '你是谁？'}],
+    )
+
+    print(completion.model_dump_json())
+
+    import os
+    from openai import OpenAI
+
+    client = OpenAI(
+        # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
+        api_key="sk-f7f3039f52e3402bbafda926f4da7cb3",
+        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    )
+    completion = client.chat.completions.create(
+        model="qwen-vl-max",
+        # 此处以qwen-vl-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+        messages=[{"role": "user", "content": [
+            {"type": "text", "text": "这是什么"},
+            {"type": "image_url",
+             "image_url": {"url": "https://dashscope.oss-cn-beijing.aliyuncs.com/images/dog_and_girl.jpeg"}}
+        ]}]
+    )
+    print(completion.model_dump_json())
